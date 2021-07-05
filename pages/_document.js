b2aa31c17e9664d41d84f6a42aa7cf3e8bf78e16
Rohit Singh <rohit99.ind@gmail.com>
2021-07-05 09:56:33 +0530

    feat: nb examples

--- pages/_document.js
+++ pages/_document.js
@@ -1,37 +1,40 @@
-import { Children } from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { AppRegistry } from 'react-native'
-import config from '../app.json'
-// Force Next-generated DOM elements to fill their parent's height
-const normalizeNextElements = `
-  #__next {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Ionicons from "react-native-vector-icons/Fonts/Ionicons.ttf";
+import MaterialIcons from "react-native-vector-icons/Fonts/MaterialIcons.ttf";
+import AntDesign from "react-native-vector-icons/Fonts/AntDesign.ttf";
+import MaterialCommunityIcons from "react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf";
+// Generate required css
+const iconFontStyles = `@font-face {
+  src: url(${Ionicons});
+  font-family: Ionicons;
+}
+@font-face {
+  src: url(${MaterialIcons});
+  font-family: MaterialIcons;
+}@font-face {
+  src: url(${AntDesign});
+  font-family: AntDesign;
+}@font-face {
+  src: url(${MaterialCommunityIcons});
+  font-family: MaterialCommunityIcons;
+}`;
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
-`
-
-export default class MyDocument extends Document {
-  static async getInitialProps({ renderPage }) {
-    AppRegistry.registerComponent(config.name, () => Main)
-    const { getStyleElement } = AppRegistry.getApplication(config.name)
-    const page = await renderPage()
-    const styles = [
-      <style dangerouslySetInnerHTML={{ __html: normalizeNextElements }} />,
-      getStyleElement(),
-    ]
-    return { ...page, styles: Children.toArray(styles) }
-  }
-
   render() {
     return (
-      <Html style={{ height: '100%' }}>
-        <Head />
-        <body style={{ height: '100%', overflow: 'hidden' }}>
+      <Html>
+        <Head>
+          <style>{iconFontStyles}</style>
+        </Head>
+        <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+// @generated: @expo/next-adapter@2.1.82
+import React from "react";
+import {
+  Center,
+  NativeBaseProvider,
+  VStack,
+  Heading,
+  Link,
+  List,
+} from "native-base";
 
-export default function App(props) {
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text accessibilityRole="header" style={styles.text}>
-        React Native for Web & Next.js
-      </Text>
-
-      <Text style={styles.link} accessibilityRole="link" href={`/alternate`}>
-        A universal link
-      </Text>
-
-      <View style={styles.textContainer}>
-        <Text accessibilityRole="header" aria-level="2" style={styles.text}>
-          Subheader
-        </Text>
-      </View>
-    </View>
-  )
+    <NativeBaseProvider>
+      <Center _web={{ h: "100vh" }}>
+        <VStack space={5}>
+          <Heading color="coolGray.800">NativeBase Examples</Heading>
+          <List.Ordered borderWidth={0}>
+            <List.Item>
+              <Link href="/animation">Animation</Link>
+            </List.Item>
+            <List.Item>
+              <Link href="/colormode">Color Mode</Link>
+            </List.Item>
+            <List.Item>
+              <Link href="/forms">Forms</Link>
+            </List.Item>
+            <List.Item>
+              <Link href="/interactions">Interactions</Link>
+            </List.Item>
+            <List.Item>
+              <Link href="/sample">Sample App</Link>
+            </List.Item>
+          </List.Ordered>
+        </VStack>
+      </Center>
+    </NativeBaseProvider>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flexGrow: 1,
-    justifyContent: 'center',
-  },
-  link: {
-    color: 'blue',
-  },
-  textContainer: {
-    alignItems: 'center',
-    marginTop: 16,
-  },
-  text: {
-    alignItems: 'center',
-    fontSize: 24,
-    marginBottom: 24,
-  },
-})
